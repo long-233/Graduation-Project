3fbfe9f21f6930be8016c67cec5a3612a7020dd1
long <q504738673@sina.com>
2018-04-17 07:30:30 +0800

    commit

--- www/models.py
+++ www/models.py
@@ -3,12 +3,12 @@ import time, uuid
 from orm import Model, StringField, BooleanField, FloatField, TextField
 
 def next_id():
-    return '%015d%s000' % (int(time() * 1000), uuid.uuid4().hex)
+    return '%015d%s000' % (int(time.time() * 1000), uuid.uuid4().hex)
 
 class User(Model):
     __table__ = 'users'
 
-    id = StringField(primary_key=True, default=nexe_id, ddl='varchar(50)')
+    id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
     email = StringField(ddl='varchar(50)')
     passwd = StringField(ddl='varchar(50)')
     admin = BooleanField()
