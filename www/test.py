e754d8a758bbe6466de7b92455c6399e70ca21e1
long <q504738673@sina.com>
2018-04-17 07:31:03 +0800

    commit

--- www/test.py
+++ www/test.py
@@ -1,7 +1,15 @@
-from orm import Model, StringField, IntegerField
+import orm
+from models import User, Blog, Comment
+import asyncio
 
-class User(Model):
-    __table__ = 'users'
+loop = asyncio.get_event_loop()
 
-    id = IntegerField(primary_key=True)
-    name = StringField()
+async def test():
+    # 连接数据库
+    await orm.create_pool(loop=loop, host='127.0.0.1', port=3306, user='root', password='123456', db='awesome')
+
+    u = User(name='Test', email='test@example.com', passwd='1234567890', image='about:blank')
+
+    await u.save()
+
+loop.run_until_complete(test())
